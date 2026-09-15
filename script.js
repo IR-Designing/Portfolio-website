b26904ef7b160bfcc9c342761ca13053f5ac57fd
IR-CODER <irdesigning77@gmail.com>
2026-09-15 12:00:10 +0500

    changes in the animation

--- script.js
+++ script.js
@@ -37,25 +37,41 @@ changeText();
 setInterval(changeText,2000)
 
 // circle.skills--------------------------------------------------------->
-const circle = document.querySelectorAll(".circle");
-circle.forEach(elem=>{
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
-    var points = "";
-    var rotate = 360 / dots;
+// Technical skill bars
+document.querySelectorAll(".bar span").forEach((bar) => {
+    const percent = Number(bar.dataset.percent);
+    bar.style.setProperty("--percent", percent);
 
+    setTimeout(() => {
+        bar.classList.add("animate");
+    }, 200);
+});
 
-    for(let i = 0; i < dots ; i++){
-        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg" style="--i:1;"></div>`
+// Professional circular skills
+document.querySelectorAll(".circle").forEach((circle) => {
+    const dots = Number(circle.dataset.dots);
+    const percent = Number(circle.dataset.percent);
+    const markedDots = Math.round((dots * percent) / 100);
+    const rotation = 360 / dots;
+
+    let points = "";
+
+    for (let i = 0; i < dots; i++) {
+        points += `
+            <div class="points"
+                style="--i: ${i}; --rot: ${rotation}deg;">
+            </div>
+        `;
     }
-    elem.innerHTML = points;
 
-    const pointsMarked = elem.querySelectorAll(".points");
-    for(let i = 0; i < percent; i++){
-        pointsMarked[i].classList.add("marked")
+    circle.innerHTML = points;
+
+    const allPoints = circle.querySelectorAll(".points");
+
+    for (let i = 0; i < markedDots; i++) {
+        allPoints[i].classList.add("marked");
     }
-})
+});
 
 
 // mixed it up Portfolio Section--------------------------------------------------------->
